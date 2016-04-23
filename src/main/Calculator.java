bd0ece0c986bb8c3dff4c1900a37876ea0de3ecc
Nic <uth.sentient@gmail.com>
2016-04-23 02:24:01 -0600

    + added simple gui * unfinished state

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,14 +1,47 @@
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.util.Scanner;
 
-public class Calculator {
+import javax.swing.*;
 
+public class Calculator {
+	
+	
+	private JFrame f;	
+	private JPanel p;
+	private JButton b1;
+	private JLabel lab;
+	
+	public Calculator() {
+		gui();
+	}
+	
+	public void gui() {
+		f = new JFrame("Calculator");
+		f.setVisible(true);
+		f.setSize(500, 500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		p = new JPanel();
+		p.setBackground(Color.LIGHT_GRAY);
+		
+		b1 = new JButton("Click Me");
+		lab = new JLabel("DONT CLICk ME");
+		
+		p.add(b1, BorderLayout.CENTER);
+		p.add(lab);
+		
+		f.add(p);
+	}
+	
 	public static void main (String[] args) {
+		
+		new Calculator(); 
 		
 		Scanner sc = new Scanner(System.in);
 		int num;
-		
 		
 		System.out.println("Enter a number for conversion: ");
 		num = sc.nextInt();
