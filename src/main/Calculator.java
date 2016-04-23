af44c2d5657187eae06f70d6cc1288ab668330a5
Nic <uth.sentient@gmail.com>
2016-04-23 00:58:04 -0600

    = cleaned the large conversion logic

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -17,69 +17,38 @@ public class Calculator {
 		
 		preNum = num;
 		
-		if (num <= 128) {
-			while (num > 0) {
-				if (num == 128) {
-					binary[0] = 1;
-					num -= 128;
-				} else if (num >= 64) {
-					binary[1] = 1;
-					num -= 64;
-				} else if (num >= 32) {
-					binary[2] = 1;
-					num -= 32;
-				} else if (num >= 16) {
-					binary[3] = 1;
-					num -= 16;
-				} else if (num >= 8) {
-					binary[4] = 1;
-					num -= 8;
-				} else if (num >= 4) {
-					binary[5] = 1;
-					num -= 4;
-				} else if (num >= 2) {
-					binary[6] = 1;
-					num -= 2;
-				} else if (num >= 1) {
-					binary[7] = 1;
-					num -= 1;
-				}
-			}
-		} else {
+		while (num > 0) {
 			while (num > 128) {
 				num -= 128;
 				numOut++;
 			}
 			
-			while (num > 0) {
-				if (num == 128) {
-					binary[0] = 1;
-					num -= 128;
-				} else if (num >= 64) {
-					binary[1] = 1;
-					num -= 64;
-				} else if (num >= 32) {
-					binary[2] = 1;
-					num -= 32;
-				} else if (num >= 16) {
-					binary[3] = 1;
-					num -= 16;
-				} else if (num >= 8) {
-					binary[4] = 1;
-					num -= 8;
-				} else if (num >= 4) {
-					binary[5] = 1;
-					num -= 4;
-				} else if (num >= 2) {
-					binary[6] = 1;
-					num -= 2;
-				} else if (num >= 1) {
-					binary[7] = 1;
-					num -= 1;
-				}
+			if (num == 128) {
+				binary[0] = 1;
+				num -= 128;
+			} else if (num >= 64) {
+				binary[1] = 1;
+				num -= 64;
+			} else if (num >= 32) {
+				binary[2] = 1;
+				num -= 32;
+			} else if (num >= 16) {
+				binary[3] = 1;
+				num -= 16;
+			} else if (num >= 8) {
+				binary[4] = 1;
+				num -= 8;
+			} else if (num >= 4) {
+				binary[5] = 1;
+				num -= 4;
+			} else if (num >= 2) {
+				binary[6] = 1;
+				num -= 2;
+			} else if (num >= 1) {
+				binary[7] = 1;
+				num -= 1;
 			}
 		}
-		
 		
 		if(preNum > 128) {
 			for(int i = 0; i < numOut; i++) {
@@ -94,8 +63,6 @@ public class Calculator {
 				System.out.print(binary[i]);
 			}
 		}
-		
-		
 		
 		sc.close();
 	}
