024155784190ad29fcdbb8163f4a590c10438fd4
Nic <uth.sentient@gmail.com>
2016-04-23 00:32:13 -0600

    + gets the initial input

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -7,6 +7,11 @@ public class Calculator {
 	public static void main (String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		
+		System.out.println("Enter a number for conversion: ");
+		num = sc.nextInt();
 		
 		sc.close();
 	}
