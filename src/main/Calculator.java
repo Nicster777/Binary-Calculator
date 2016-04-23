6d69671e00d75753ad14865520c9a1b473607317
Nic <uth.sentient@gmail.com>
2016-04-23 00:37:54 -0600

    + added basic structure for binary conversion

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -7,11 +7,50 @@ public class Calculator {
 	public static void main (String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
+		int[] binary = new int[8]; // 0 0 0 0 0 0 0 0
+		
 		int num;
 		
 		
 		System.out.println("Enter a number for conversion: ");
 		num = sc.nextInt();
+		
+		while (num > 0) {
+			while (num >= 128) {
+				binary[0] = 1;
+				num -= 128;
+			}
+			
+			while (num >= 64) {
+				
+			}
+			
+			while (num >= 32) {
+				
+			}
+			
+			while (num >= 16) {
+				
+			}
+			
+			while (num >= 8) {
+				
+			}
+			
+			while (num >= 4) {
+				
+			}
+			
+			while (num >= 2) {
+				
+			}
+			
+			while (num >= 1) {
+				
+			}
+		}
+		
 		
 		sc.close();
 	}
